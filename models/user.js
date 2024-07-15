f3e9726f71e425ed6a314cfc370b554ad2644740
tomason182 <elotro182@hotmail.com>
2024-07-15 14:37:45 -0300

    add user Schema

--- models/user.js
+++ models/user.js
@@ -1,25 +1,30 @@
 const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  first_name: { type: String, require: true, maxLength: 50},
-  last_name: { type: String, require: true, maxLength: 50},
-  email: { type: String, require: true, maxLength: 50},
-  password: { type: String, require: true, maxLength: 50},
-  member_status: 
-})
+  first_name: { type: String, required: true, maxLength: 50 },
+  last_name: { type: String, required: true, maxLength: 50 },
+  email: { type: String, required: true, maxLength: 50 },
+  password: { type: String, required: true, maxLength: 50 },
+  member_status: {
+    type: String,
+    required: true,
+    enum: ["admin", "full", "normal"],
+    default: "normal",
+  },
+});
 
 // Virtual for user full name
 UserSchema.virtual("full_name").get(function () {
   let fullName = "";
-  if(this.first_name && this.last_name) {
-    fullName = `${this.last_name}, ${this.first_name}`
+  if (this.first_name && this.last_name) {
+    fullName = `${this.last_name}, ${this.first_name}`;
   }
   return fullName;
-})
+});
 
 // Virtual for user URL
 UserSchema.virtual("url").get(function () {
-  return `/profile/user/${this._id}`
-})
+  return `/profile/user/${this._id}`;
+});
