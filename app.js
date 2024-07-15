8fdbcae50ae3d06f4c1090f95bec3844f376cfd6
tomason182 <elotro182@hotmail.com>
2024-07-15 16:46:12 -0300

    add user routes to main app

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ require("dotenv").config();
 const express = require("express");
 const port = process.env.PORT || 3000;
 const main = require("./config/db");
-const app = express();
+const userRoutes = require("./routes/users");
 
 // Connect to database
 main().catch((err) => console.log(`Error connecting with database: ${err}`));
 
-app.get("/", (req, res) => {
-  res.send("Hello, World");
-});
+// Initialize express application
+const app = express();
+
+app.use("/users", userRoutes);
 
 app.listen(port, () => console.log(`Server started at port ${port}`));
---
+++ routes/users.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+
+// Require controllers module
+const user_controller = require("../controllers/userControllers");
+
+/// USER ROUTES ///
+
+// GET request for register a new User
+router.get("/register", user_controller.user_registration_get);
+
+// POST request for register a new User
+router.post("/register", user_controller.user_registration_post);
+
+// GET request for logIn a User
+router.get("/login", user_controller.user_login_get);
+
+// POST request for logIng a User
+router.post("/login", user_controller.user_login_post);
+
+// GET request for display User Profile.
+router.get("/profile/:id", user_controller.user_profile);
+
+module.exports = router;
