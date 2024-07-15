49f257c0446d8696865f204a4834a6b06169d862
tomason182 <elotro182@hotmail.com>
2024-07-15 17:09:29 -0300

    add new messageController

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -8,23 +8,45 @@ exports.message_display_all = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: Display all messages");
 });
 
+/// CREATE A NEW MESSAGE
+
 // @desc    Create a message
-// @route   POST /messages
+// @route   GET /messages/create
 // @access  Private
-exports.message_create = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Create a msj");
+exports.message_create_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Create a message on GET");
 });
+
+// @desc    Create a message
+// @route   POST /messages/create
+// @access  Private
+exports.message_create_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Create a message on POST");
+});
+
+/// RETRIEVE AN SPECIFIC MESSAGE ///
+
+// @desc    Get an specific message by id
+// @route   GET /messages/:id
+// @access  Private
+exports.message_display_one = asyncHandler(async (req, res, next) => {
+  res.send(`NOT IMPLEMENTED: Retrieve message with id: ${req.params.id}`);
+});
+
+/// EDIT A MESSAGE ///
 
 // @desc    Edit a message
 // @route   PUT /messages/:id
 // @access  Private
-exports.message_edit = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Edit a msj");
+exports.message_edit_post = asyncHandler(async (req, res, next) => {
+  res.send(`NOT IMPLEMENTED: Edit message ${req.params.id}`);
 });
+
+/// DELETE A MESSAGE ///
 
 // @desc    Delete a message
 // @route   DELETE /messages/:id
 // @access  Private
 exports.message_delete = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Delete a msj");
+  res.send(`NOT IMPLEMENTED: Delete the message ${req.params.id}`);
 });
