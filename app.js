bf576646f307c0688b0c01a8df8cc7008d359f25
tomason182 <elotro182@hotmail.com>
2024-07-15 18:00:32 -0300

    add view engine set up

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const errorHandler = require("./middleware/errorMiddleware");
+const path = require("node:path");
 const port = process.env.PORT || 3000;
 const main = require("./config/db");
 
@@ -14,6 +15,13 @@ main().catch((err) => console.log(`Error connecting with database: ${err}`));
 
 // Initialize express application
 const app = express();
+
+// View engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/users", userRoutes);
 app.use("/message", messageRoutes);
